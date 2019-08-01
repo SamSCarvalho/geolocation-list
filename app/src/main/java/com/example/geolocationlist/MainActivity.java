7bd9ea7bd67a9a1f02bbb4be323af729f9f43b0a
Samuel Carvalho <sam.scarvalho17@gmail.com>
2019-07-31 21:45:17 -0300

    first commit

---
+++ app/src/main/java/com/example/geolocationlist/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.geolocationlist;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        PageAdapter pageAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pageAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/geolocationlist/PageAdapter.java
@@ -0,0 +1,35 @@
+package com.example.geolocationlist;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class PageAdapter extends FragmentStatePagerAdapter {
+    int countTab;
+    public PageAdapter(FragmentManager fm, int countTab) {
+        super(fm);
+        this.countTab = countTab;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                TabOne tabOne = new TabOne();
+                return tabOne;
+            case 1:
+                TabTwo tabTwo = new TabTwo();
+                return tabTwo;
+            case 2:
+                TabThree tabThree = new TabThree();
+                return tabThree;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return countTab;
+    }
+}
