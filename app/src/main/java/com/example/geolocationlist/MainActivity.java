90533a5f84020bb0dc5c92488890ab40922ea3cf
Samuel Carvalho <sam.scarvalho17@gmail.com>
2019-07-31 22:43:17 -0300

    Adicionado o LocationManager e solicitação de permissão para acesso do GPS

--- app/src/main/java/com/example/geolocationlist/MainActivity.java
+++ app/src/main/java/com/example/geolocationlist/MainActivity.java
@@ -1,18 +1,41 @@
 package com.example.geolocationlist;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
 
-public class MainActivity extends AppCompatActivity{
+import java.util.ArrayList;
+import java.util.List;
 
+
+public class MainActivity extends AppCompatActivity{
+    private LocationManager locationManager;
+    private static final int REQUEST_LOCATION = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if(ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION);
+        }
+        List<String> list = locationManager.getAllProviders();
+        Log.d("get_location", list.toString());
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
--- app/src/main/java/com/example/geolocationlist/TabOne.java
+++ app/src/main/java/com/example/geolocationlist/TabOne.java
@@ -1,5 +1,6 @@
 package com.example.geolocationlist;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
